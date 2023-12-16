e1d5dd1564d2c0fc1854187185b6601a243af82d
Jenna D <200529678@student.georgianc.on.ca>
2023-12-16 14:01:21 -0500

    File Setup first commit

---
+++ src/views/shared/Header.js
@@ -0,0 +1,48 @@
+
+function Header (){
+    return(
+       <nav className="navbar navbar-expand-lg navbar-dark shadow">
+      <a className="navbar-brand mx-4" href="/" id="home">
+        Jenna Deamer
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link" href="/about" id="about">
+              About
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/projects" id="projects">
+              Projects
+            </a>
+          </li>
+          {/* <li className="nav-item">
+            <a className="nav-link" href="/">
+              Skills
+            </a>
+          </li> */}
+          <li className="nav-item">
+            <a className="nav-link" href="/contact" id="contact">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+      
+    )
+}
+
+export default Header;
