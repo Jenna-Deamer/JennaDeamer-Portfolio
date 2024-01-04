92e546715cc7bc115eab1b457034396f1d02d0a5
Jenna D <200529678@student.georgianc.on.ca>
2024-01-03 22:12:18 -0500

    added google fonts, Changed body text size to 18px

--- src/views/Home.js
+++ src/views/Home.js
@@ -4,7 +4,7 @@ function Home() {
     <section className="pageContainer">
         <div className="text-center"> 
           <h1>Hello, I'm Jenna Deamer</h1>
-          <h2 className="pb-2">Dedicated to Building Engaging and Accessible Web Applications</h2>
+          <h2 className="pb-4">Dedicated to Building Engaging and Accessible Web Applications</h2>
           <a className="btnPrimary me-3"  href="/projects">View Work</a>
           <a className="btnPrimary" href="/about" >About Me</a>
         </div>
