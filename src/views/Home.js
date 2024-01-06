8e796469bb805230d9434c3f0ab8b3dd98247e1a
Jenna D <200529678@student.georgianc.on.ca>
2024-01-06 18:54:40 -0500

    changed header font, better line spacing, bolded h1 hero

--- src/views/Home.js
+++ src/views/Home.js
@@ -4,8 +4,8 @@ function Home() {
     <>
     <section className="pageContainer">
         <div className="text-center"> 
-          <h1>Hello, I'm Jenna Deamer</h1>
-          <h2 className="pb-4" id="heroHeader">Dedicated to Building Engaging and Accessible Web Applications</h2>
+          <h1 id="heroHeader">Hello, I'm Jenna Deamer</h1>
+          <h2 className="pb-4" id="heroSubHeader">Dedicated to Building Engaging and Accessible Web Applications</h2>
           <a className="btnPrimary me-3"  href="/projects">View Work</a>
           <a className="btnSecondary" href="/about" >About Me</a>
         </div>
