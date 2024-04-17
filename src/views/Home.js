32197103286f36e8c12ee7863cd67a57af96c38c
Jenna Deamer <jennadeamer@gmail.com>
2024-04-17 15:34:54 -0400

    fixed button sizes

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,16 +1,23 @@
-
 function Home() {
   return (
     <>
-    <section className="pageContainer">
-        <div className="text-center"> 
+      <section className="pageContainer">
+        <div className="text-center">
           <h1 id="heroHeader">Hello, I'm Jenna Deamer</h1>
-          <h2 className="pb-4" id="heroSubHeader">Crafting Engaging and User-Centric Experiences</h2>
-          <a className="btnPrimary me-3"  href="/projects">View Work</a>
-          <a className="btnSecondary" href="/about" >About Me</a>
-        </div>
-        </section>
+          <h2 className="pb-4" id="heroSubHeader">
+            Crafting Engaging and User-Centric Experiences
+          </h2>
 
+          <div className="buttonsWrapper">
+            <a className="btnPrimary me-3" href="/projects">
+              View Work
+            </a>
+            <a className="btnSecondary" href="/about">
+              About Me
+            </a>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
