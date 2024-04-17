c0b7da91237ebbebabb13bc86c5e38ae9701fbe8
Jenna Deamer <jennadeamer@gmail.com>
2024-04-16 22:24:16 -0400

    Projects alternating extra row bug

--- src/views/Projects.js
+++ src/views/Projects.js
@@ -1,7 +1,3 @@
-import React, { useState, useEffect } from "react";
-import Modal from "react-modal";
-
-Modal.setAppElement("#root"); //Accessibility
 function Projects() {
   const projectCards = [
     {
@@ -18,103 +14,68 @@ function Projects() {
       title: "Automoblox Redesign",
       imgUrl: "/images/thumbnails/AutomobloxThumbnail.webp",
       imgAlt: "Automoblox Redesign Thumbnail",
-      description: "Transformed the classic Automoblox website with improved user experience and modern design.",
+      description:
+        "Transformed the classic Automoblox website with improved user experience and modern design.",
       skillText: "HTML | CSS | Javascript | Bootstrap",
       githubUrl: "https://github.com/Jenna-Deamer/Automoblox-Redesign",
       demoUrl: "https://automoblox-comp2081.netlify.app/",
     },
-  
-
+    {
+      title: "Automoblox Redesign",
+      imgUrl: "/images/thumbnails/AutomobloxThumbnail.webp",
+      imgAlt: "Automoblox Redesign Thumbnail",
+      description:
+        "Transformed the classic Automoblox website with improved user experience and modern design.",
+      skillText: "HTML | CSS | Javascript | Bootstrap",
+      githubUrl: "https://github.com/Jenna-Deamer/Automoblox-Redesign",
+      demoUrl: "https://automoblox-comp2081.netlify.app/",
+    }
   ];
-
-  //Mange hoverState for each card independently by using an array.
-  const [hoverStates, setHoverStates] = useState({});
-  //When function is called, Pass in  the index of the card hovered/un-hovered & update state
-  const handleMouseEnter = (index) => {
-    setHoverStates({ hoverStates, [index]: true });
-  };
-  const handleMouseLeave = (index) => {
-    setHoverStates({ hoverStates, [index]: false });
-  };
-  //track modal open & close state
-  // const [isModalOpen, setIsModalOpen] = useState({});
-  // const modalOpen = (index) => {
-  //   setIsModalOpen({ isModalOpen, [index]: true });
-  //   document.body.style.overflow = "hidden"; // Prevent scrolling on open modal
-  // };
-  // const modalClose = (index) => {
-  //   setIsModalOpen({ isModalOpen, [index]: false });
-  //   document.body.style.overflow = "auto"; // Enable scrolling on modal close
-  // };
 
   return (
     <>
-      <section className="container pageContainer row mb-4" id="projectsContainer">
+      <section className="container mb-4" id="projectsContainer">
         {projectCards.map((card, index) => (
-          <article key={index} className="col-lg-6 projectCard">
-            <h2>{card.title}</h2>
-            <a
-              href={card.demoUrl}
-              target="_blank"
-              rel="noopener noreferrer"
-              onMouseEnter={() => handleMouseEnter(index)}
-              onMouseLeave={() => handleMouseLeave(index)}
-              className={hoverStates[index] ? "hovered" : ""}
-            >
-              {/** When hoverState is true for a card, Apply hovered CSS class.
-               * Updates to false when the mouseLeaves, removing the CSS class */}
+          <article key={index} className="projectCard">
+              <h2 className="projectTitle">{card.title}</h2>
+            <div className="ProjectImageSide">
+    
               <img
                 src={card.imgUrl}
                 alt={card.imgAlt}
                 className="pb-2 img-fluid"
               />
-              {hoverStates[index] && (
-                <span className="linkText">Click to view live demo</span>
-              )}
-            </a>
+              <div className="buttonsWrapper pt-2">
+                <a
+                  href={card.demoUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="btnPrimary"
+                  aria-label="Visit the live demo"
+                >
+                  <i className="bi bi-laptop"></i> Demo
+                </a>
+                <a
+                  href={card.githubUrl}
+                  className="btnSecondary"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="Visit the github repository"
+                >
+                  <i className="bi bi-github"></i> Github
+                </a>
+              </div>
+            </div>
+
+            <div className="projectDescSide">
             <p>{card.description}</p>
             <small>{card.skillText}</small>
-            <div className="buttonsWrapper pt-2">
-              <a
-                href={card.githubUrl}
-                className="btnPrimary"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="Visit the github repository"
-              >
-                <i className="bi bi-github"></i> Github
-              </a>
-
-              {/* <button
-                className="btnSecondary"
-                aria-label="opens a window on this page that contains more information about this project"
-                onClick={() => modalOpen(index)}
-              >
-                <i className="bi bi-file-earmark-code-fill"></i> More Detials
-              </button> */}
             </div>
-            {/* 
-            <Modal
-              isOpen={isModalOpen[index]}
-              onRequestClose={() => modalClose(index)}
-              style={{
-                overlay: {
 
-                },
-                content: {
-                  margin: 'auto',
-                },
-              }}
-            >
-              <h2 className="text-center">{card.title}</h2>
-              
-
-              <button onClick={() => modalClose(index)}>Close</button>
-            </Modal> */}
-
+           
           </article>
         ))}
-      </section >
+      </section>
     </>
   );
 }
