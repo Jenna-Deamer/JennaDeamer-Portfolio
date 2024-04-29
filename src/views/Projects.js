797c272bcbe0ea1569ce2dba6dc85382c547214a
Jenna Deamer <jennadeamer@gmail.com>
2024-04-28 20:05:35 -0400

    Added github link to smart beach project section

--- src/views/Projects.js
+++ src/views/Projects.js
@@ -7,7 +7,7 @@ function Projects() {
       description:
         "Smart Beach was a group project dedicated to enhancing safety at Station Beach in Kincardine, Ontario. Leveraging machine learning models and APIs developed by students specializing in Machine Learning and Big Data at Georgian College, our team crafted a predictive-based solution accessible through a user-friendly app interface. By integrating real-time data on water and weather conditions, our solution empowers beach visitors with critical insights for a safer, more enjoyable experience. ",
       skillList: ["Vite", "React","CSS","Bootstrap","HTML"],
-      // githubUrl: "https://github.com/C-Blouin/SmartBeach",
+      githubUrl: "https://github.com/C-Blouin/SmartBeach",
       demoUrl: "https://smartbeach.vercel.app/",
 
     },
